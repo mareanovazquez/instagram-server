138d1ba8be9a299d2b58c69c8b2b6f269176dda0
mareanovazquez <mareanovazquez@protonmail.com>
2025-05-26 17:40:17 -0300

    add CORS para peticiones del front

--- index.js
+++ index.js
@@ -6,7 +6,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+    origin: ['http://127.0.0.1:5500', 'http://localhost:5500', 'https://epicaserviciosturisticos.com.ar'],
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
 app.use(express.json());
 
 // Ruta principal de prueba
